8373ec0526233c6ce1cd3abf39280c218a02b940
Vivekanand0109 <130480227+Vivekanand0109@users.noreply.github.com>
2025-06-10 23:14:48 +0530

    PrimeApiApplication.java

---
+++ PrimeApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.primeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+public class PrimeApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeApiApplication.class, args);
+    }
+}
